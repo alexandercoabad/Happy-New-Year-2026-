b5c9802333b056937b6b494f184d54ad56d97ed4
alexandercoabad <144318238+alexandercoabad@users.noreply.github.com>
2025-12-27 10:41:09 +0800

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -29,7 +29,9 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_HappyNewYear2026 (
+  //tt_um_HappyNewYear2026 (
+   tt_um_HappyNewYear2026 user_project (
+
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
